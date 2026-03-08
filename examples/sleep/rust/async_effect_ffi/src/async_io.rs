e3e4c3b1fe2d3828575642b2c18b4ce7f3949411
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2026-03-08 19:44:28 -0400

    Add Rust FFI function for AsyncIO monadic bind

--- examples/sleep/rust/async_effect_ffi/src/async_io.rs
+++ examples/sleep/rust/async_effect_ffi/src/async_io.rs
@@ -17,6 +17,9 @@ use internal_lean_object::InternalLeanObject;
 ///
 /// This type assumes that it will not be used as a persistent value by Lean
 /// code, because it marks Lean objects as multi-threaded.
+// The inner field must be [`Some`] at all times. It is an [`Option`] only
+// because [`AsyncIo`] methods take `self` by value whereas the Lean runtime
+// always passes external objects to functions by reference.
 #[derive(Clone, LeanExternalTypeTag)]
 pub struct LeanAsyncIo(Option<AsyncIo<InternalLeanObject>>);
 
@@ -59,6 +62,27 @@ impl ExternalClassHolder for LeanAsyncIo {
     }
 }
 
+/// Destructively update a [`LeanAsyncIo`]
+///
+/// # Safety
+///
+/// Callers must ensure that:
+/// 1. `instance` has an associated reference counting token
+/// 2. `instance` is a Lean external object containing a [`LeanAsyncIo`]
+///    object
+unsafe fn clone_on_write_async_io<
+    F: FnOnce(AsyncIo<InternalLeanObject>) -> AsyncIo<InternalLeanObject>,
+>(
+    f: F,
+    instance: lean_obj_arg,
+) -> lean_obj_res {
+    let mut instance_object: Object<LeanAsyncIo> = unsafe { Object::new(instance) };
+    let mapped_object = instance_object
+        .clone_on_write(move |lean_async_io| lean_async_io.0 = lean_async_io.0.take().map(f))
+        .unwrap_or(instance_object);
+    mapped_object.into_raw()
+}
+
 /// Create an instance storing a value
 ///
 /// # Safety
@@ -84,17 +108,48 @@ pub unsafe extern "C" fn async_effect_ffi_async_io_map(
     f: lean_obj_arg,
     instance: lean_obj_arg,
 ) -> lean_obj_res {
-    let safe_f = unsafe { InternalLeanObject::new(f) };
-    let mut instance_object: Object<LeanAsyncIo> = unsafe { Object::new(instance) };
-    let mapped_object = instance_object
-        .clone_on_write(move |lean_async_io| {
-            lean_async_io.0 = lean_async_io.0.take().map(move |async_io| {
-                async_io.map(move |value| unsafe {
+    unsafe {
+        let safe_f = InternalLeanObject::new(f);
+        clone_on_write_async_io(
+            move |async_io| {
+                async_io.map(move |value| {
                     let res = lean_apply_1(safe_f.clone().into_raw(), value.into_raw());
                     InternalLeanObject::new(res)
                 })
-            })
-        })
-        .unwrap_or(instance_object);
-    mapped_object.into_raw()
+            },
+            instance,
+        )
+    }
+}
+
+/// Monadic bind
+///
+/// # Safety
+///
+/// Callers must ensure that:
+/// 1. The arguments have associated reference counting tokens
+/// 2. `instance` is a Lean external object containing a [`LeanAsyncIo`] object
+/// 3. `f` is a Lean closure that accepts a single argument of the type of value
+///    currently stored by `instance` and returns a Lean external object
+///    containing a [`LeanAsyncIo`] object
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn async_effect_ffi_async_io_bind(
+    instance: lean_obj_arg,
+    f: lean_obj_arg,
+) -> lean_obj_res {
+    unsafe {
+        let safe_f = InternalLeanObject::new(f);
+        clone_on_write_async_io(
+            move |async_io| {
+                async_io.bind(move |value| {
+                    let res = lean_apply_1(safe_f.clone().into_raw(), value.into_raw());
+                    // It is impossible to move a value out of a Lean-managed
+                    // object, so clone instead
+                    let res_object: LeanAsyncIo = Object::new(res).clone_inner();
+                    res_object.0.unwrap()
+                })
+            },
+            instance,
+        )
+    }
 }
